6f890fd2e99adab7bff0a169f53ea277b59c1f93
hardiksuthar74 <hardiksuthar74@gmail.com>
2023-10-01 14:53:43 +0530

    map added

--- js/contactAnimation.js
+++ js/contactAnimation.js
@@ -9,3 +9,16 @@ const newBubbleObserver = new IntersectionObserver((entries) => {
 const hiddenBubbleEl = document.querySelectorAll(".hidden-animation");
 
 hiddenBubbleEl.forEach((el) => newBubbleObserver.observe(el));
+
+var map = L.map("map").setView([19.0821526, 72.9026632], 17);
+map.scrollWheelZoom.disable();
+
+L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([19.0821526, 72.9026632])
+  .addTo(map)
+  .bindPopup("Top Painting India")
+  .openPopup();
